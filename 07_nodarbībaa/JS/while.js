d8304626bd6a820c00aaadf70d04eb309095dc75
RonaldsSimkus <ronalds1905@gmail.com>
2023-05-11 19:07:00 +0300

    110523

--- 07_nodarbībaa/JS/while.js
+++ 07_nodarbībaa/JS/while.js
@@ -1,7 +1,7 @@
 let text = "";
 let i = 0;
-while (i < 10) {
-  text += "<br>" + i;
+while (i < 20) {
+  if (i % 2 == 0) text += "<br>" + i;
   i++;
 }
 document.getElementById("count").innerHTML = text;
