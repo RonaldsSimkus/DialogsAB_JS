f11a0e6cf14fb3691051cc0fcabcc63d694be5fe
RonaldsSimkus <ronalds1905@gmail.com>
2023-05-13 19:59:47 +0300

    130523

---
+++ 08_nodarbība/JS/objects.js
@@ -0,0 +1,16 @@
+const discrete_variable = 4;
+const array_example = ["a", "b"];
+const function_example = function name_for_internal_use(x) {
+  return x * x;
+};
+
+const car = { type: "Fiat", model: "500", color: "white" };
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
