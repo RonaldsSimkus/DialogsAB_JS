493212a57a3df1a09ebe4ae64826d08d498eea68
RonaldsSimkus <ronalds1905@gmail.com>
2023-05-16 20:04:17 +0300

    160523

--- 08_nodarbība/JS/objects.js
+++ 08_nodarbība/JS/objects.js
@@ -14,3 +14,30 @@ const person = {
     return this.firstName + " " + this.lastName;
   },
 };
+
+var discrete_variable_new = discrete_variable;
+const array_example_new = array_example;
+
+const car_1 = car;
+car_1.type = "Seat";
+console.log(car_1.type);
+console.log(car.type);
+
+const person_1 = new Object();
+
+for (let i = 0; i < array_example.length; i++) console.log(array_example[i]);
+
+for (array_element of array_example) console.log(array_element);
+
+for (array_element in array_example) console.log(array_element);
+for (array_element in array_example) console.log(array_example[array_element]);
+
+for (key in person) console.log(person[key]); //console.log(key);
+
+for (key in person) {
+  typeof person[key] == "function"
+    ? console.log(person[key]())
+    : console.log(person[key]);
+}
+
+//for (key of person) console.log(person[key]);//console.log(key);
